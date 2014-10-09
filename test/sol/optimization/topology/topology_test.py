6b9b20c4b61eaba72b848b5077cd7296a306ada3
Victor Heorhiadi <victor@cs.unc.edu>
2014-10-09 19:19:16 -0400

    starting the topology test; throwing in some todo flags

--- test/sol/optimization/topology/topology_test.py
+++ test/sol/optimization/topology/topology_test.py
@@ -1,3 +1,22 @@
+# coding=utf-8
+import networkx
+import os
+from sol.optimization.topology.generators import generateCompleteTopology
+from sol.optimization.topology.topology import Topology
 
-def testConstructors():
-    pass
+
+def testTopologyWriteRead(tmpdir):
+    dirname = os.path.abspath(tmpdir.dirname)
+    topo = generateCompleteTopology(5)
+    topo.writeGraph(dirname)
+    topo2 = Topology(topo.name)
+    topo2.loadGraph(dirname + os.path.sep + topo.name + '.graphml')
+    assert networkx.is_isomorphic(topo, topo2)
+    topo.writeGraph(dirname, 'lalala')
+    topo2 = Topology.loadGraph(dirname + os.path.sep + 'lalala.graphml')
+    assert networkx.is_isomorphic(topo, topo2)
+
+#todo: test constructor with diff types
+#todo: test get numnodes
+
+
