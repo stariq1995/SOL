576344cad922c4dd0797f2235c35301a8d291341
Victor Heorhiadi <victor@cs.unc.edu>
2015-02-09 14:36:50 -0500

    Adding testing script/integration to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 # coding=utf-8
 
-from distutils.core import setup
+from distutils.core import setup, Command
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
 
 setup(
     name='sol',
@@ -9,5 +25,6 @@ setup(
     packages=['sol'],
     author='Victor Heorhiadi',
     url='https://bitbucket.org/progwriter/sol',
-    requires=['networkx', 'requests', 'netaddr', 'pytest', 'numpy']
+    requires=['networkx', 'requests', 'netaddr', 'pytest', 'numpy'],
+    cmdclass={'test': PyTest},
 )
