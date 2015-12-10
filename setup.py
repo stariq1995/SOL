e892060c2c5cdf741527c27f881c59efa34d4112
Victor Heorhiadi <victor@cs.unc.edu>
2015-12-09 19:35:12 -0500

    reverting back to distutils. simpler and doesn't mess up cython/pytest

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 # coding=utf-8
 
+
 from Cython.Build import cythonize
-from setuptools import setup, Extension
+from distutils.core import setup
 
 setup(
     name='sol',
     version='0.5',
-    package_dir={'': 'src'},
-    packages=['sol'],
+    description='SOL: SDN Optimization Layer',
+
     author='Victor Heorhiadi',
     author_email='victor@cs.unc.edu',
-    description='SOL: SDN Optimization Layer',
+
+    package_dir={'': 'src'},
+    packages=['sol'],
     url='https://bitbucket.org/progwriter/sol',
     setup_requires=['pytest-runner'],
     requires=['networkx', 'requests', 'netaddr', 'pytest', 'numpy', 'cython', 'six'],
     tests_require=['pytest'],
-    ext_modules=cythonize(Extension('solc', "src/sol/**/*.pyx"))
+    ext_modules=cythonize("src/sol/**/*.pyx")
 )
