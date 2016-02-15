65cec71cb3f43af5d1c118f99d32e88a3d570cfe
Victor Heorhiadi <victor@cs.unc.edu>
2016-02-15 18:12:50 -0500

    Fixing uuid generation for apps. Adding objective pptc for apps. Generalizing parts of the composer; making functions visible to the outside.

--- src/sol/opt/app.py
+++ src/sol/opt/app.py
@@ -1,18 +1,22 @@
 import json
 import uuid
+
 from varnames import ALLOCATE_FLOW, ROUTE_ALL
 
 
 class App:
-    def __init__(self, pptc=None, resources=None, obj=None,
+    def __init__(self, pptc=None, resources=None, obj=None, objpptc=None,
                  constraints=[ALLOCATE_FLOW, ROUTE_ALL], name=''):
         self.obj = obj
         self.pptc = pptc
         self.resources = resources
         self.name = name
-        self.constraints = constraints
         if not self.name:
-            self.name = uuid.UUID()
+            self.name = uuid.uuid4()
+        self.constraints = constraints
+        self.objpptc = objpptc
+        if self.objpptc is None:
+            self.objpptc = pptc
 
     def uses(self, resource):
         return resource in self.resources
--- src/sol/opt/composer.pyx
+++ src/sol/opt/composer.pyx
@@ -1,35 +1,54 @@
-from gurobipy import quicksum, GRB, LinExpr, QuadExpr
+from gurobipy import LinExpr
 
+from sol import App
+from sol.opt.varnames import ALLOCATE_FLOW, ROUTE_ALL, xp, CAP_LINKS, CAP_NODES, SHARE_PROPORTIONAL_VOLUME, SHARE_EQUAL, \
+    SHARE_NUM_APPS
 from ..opt import getOptimization
 from ..utils.constansts import *
 from ..utils.exceptions import CompositionError
 
 #XXX: this is currently tied to Gurobi 
-def _addNamedConstraints(opt, app):
+def addNamedConstraints(opt, app):
+    opt.addDecisionVars(app.pptc)
     for c in app.constraints:
         if c == ALLOCATE_FLOW:
             opt.allocateFlow(app.pptc)
         elif c == ROUTE_ALL:
             opt.routeAll(app.pptc)
+        elif c[0] == CAP_LINKS:
+            opt.capLinks(app.pptc, *c[1:])
+        elif c[0] == CAP_NODES:
+            opt.capNodes(app.pptc, *c[1:])
 
-def compose(apps, topo, resMode, globalNodeCaps, globalLinkCaps, 
-            resourcesToShare=[]):
+def compose(apps, topo, overlapResMode, resourcesToShare, resShareMode=SHARE_PROPORTIONAL_VOLUME,
+            objShareMode=SHARE_PROPORTIONAL_VOLUME):
     opt = getOptimization('Gurobi')
     for app in apps:
         opt.addDecisionVars(app.pptc)
-        _addNamedConstraints(opt, app)
+        addNamedConstraints(opt, app)
 
-    _composeResources(apps, topo, opt, nodeCaps, linkCaps, resMode)
-    proportionalFairness(apps, topo, opt)
+    _composeResources(apps, topo, opt, overlapResMode)
+    if objShareMode == SHARE_PROPORTIONAL_VOLUME:
+        _proportionalFairness(apps, topo, opt)
+    elif objShareMode == SHARE_NUM_APPS:
+        for app in apps:
+            v = addObjVar(app, topo, opt, 1.0/len(apps))
+    elif objShareMode == SHARE_EQUAL:
+        for app in apps:
+            addObjVar(app, topo, opt, 1)
+    else:
+        raise ValueError
+    for r in resourcesToShare:
+        _proportionalFairnessResource(apps, topo, opt, r)
     return opt
 
-def _composeResources(apps, topo, opt, nodeCaps, linkCaps, mode=RES_COMPOSE_MAX):
+def _composeResources(apps, topo, opt, mode):
     resourceset = set()
     for a in apps:
         for r in a.getResourceNames():
             resourceset.add(r)
 
-    # Decide how we pick resource consumptions value
+    # Decide how we pick resource consumption values
     for r in resourceset:
         cost = []
         for app in apps:
@@ -45,17 +64,19 @@ def _composeResources(apps, topo, opt, nodeCaps, linkCaps, mode=RES_COMPOSE_MAX)
                 raise CompositionError("Resource composition conflict")
             val = cost[0]
 
+        nodeCaps = {node: topo.getResources(node)[r].capacity for node in topo.nodes() if r in topo.getResources(node)}
+        linkCaps = {link: topo.getResources(link)[r].capacity for link in topo.links() if r in topo.getResources(link)}
         for app in apps:
             if app.uses(r):
-                opt.consume(app.trafficClasses, r, val, nodeCaps, linkCaps)
+                opt.consume(app.pptc, r, val, nodeCaps, linkCaps)
 
 def _proportionalFairness(apps, topo, opt, to='volume'):
     cdef double totalVol = 0
     if to.lower() == 'volume':
-        vols = {app: sum([tc.volFlows for tc in app.trafficClasses]) for app in apps}
+        vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
         totalVol = sum(vols.values())
         for app in apps:
-            v = _addObjVar(app, topo, opt)
+            v = addObjVar(app, topo, opt)
             v.Obj = vols[app] / totalVol
         opt.getGurobiModel().update()
     else:
@@ -63,36 +84,42 @@ def _proportionalFairness(apps, topo, opt, to='volume'):
 
 def _proportionalFairnessResource(apps, topo, opt, resource, to='volume'):
     cdef double totalVol = 0
+    m = opt.getGurobiModel()
     if to.lower() == 'volume':
-        vols = {app: sum([tc.volFlows for tc in app.trafficClasses]) for app in apps}
-        totalVol = sum(vols.values())
-        # depending on what resource means write the fairness equations
-        # if the resource is portion of a global resource
-        
-        #TODO: need resource in path primitive!
+        vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
+        appToExpr = {}
         for app in apps:
-            for tc in app.pptc:
-                for pi, p in enumerate(app.pptc[tc]):
-                    if resource in p:
-                        pass
+            # m.addVar(name='{}_{}'.format(app.name, resource.name))
+            # m.update()
+            e = LinExpr()
+            pptcFiltered = {tc: [p for p in app.pptc[tc] if resource in p] for tc in app.pptc}
+            for tc in pptcFiltered:
+                for path in pptcFiltered[tc]:
+                    e.add(opt.v(xp(tc, path)))
+            appToExpr[app] = e
+        for app1, app2 in zip(appToExpr.keys(), appToExpr.keys()[1:]):
+            m.addConstr(appToExpr[app1] == (vols[app1] / vols[app2]) * appToExpr[app2],
+                        name='FairShare.{}'.format(resource.name))
+            m.update()
 
-
-def _addObjVar(app, topo, opt):
+def addObjVar(app, topo, opt, weight=0):
     if app.obj.lower() == MIN_LINK_LOAD:
-        return opt.minLinkLoad('bw', 0)
+        return opt.minLinkLoad('bw', weight)
     elif app.obj.lower() == MIN_LATENCY:
-        return opt.minLatency(topo, app.trafficClasses, 0)
+        return opt.minLatency(topo, app.objpptc, weight)
     else:
         raise CompositionError("Unknown objective")
 
-def getObjVar(app, opt):
+def getObjVar(app, opt, value=False):
     if app.obj.lower() == MIN_LINK_LOAD:
-        return opt.getMaxLinkLoad('bw', False)
+        return opt.getMaxLinkLoad('bw', value)
     elif app.obj.lower() == MIN_LATENCY:
-        return opt.getLatency(False)
+        return opt.getLatency(value)
     else:
         raise CompositionError("Unknown objective")
 
+def pinTraffic(app1, app2, double ratio):
+    raise NotImplemented
 
 # cpdef maxMinRatio(apps, topo, opt):
 #     m = opt.getGurobiModel()
@@ -117,7 +144,7 @@ def getObjVar(app, opt):
 #     m = opt.getGurobiModel()
 #     # print apps
 #     for app in apps:
-#         _addObjVar(app, topo, opt)
+#         addObjVar(app, topo, opt)
 #     o = LinExpr()
 #     o.add(len(apps) * quicksum([getObjVar(app2, opt) for app2 in apps]))
 #     for app in apps:
