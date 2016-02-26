829faad2c6754607b40473b7c589476198298e4c
Victor Heorhiadi <victor@cs.unc.edu>
2016-02-26 18:06:54 -0500

    Adding some tests for the python helper

--- test/sol/utils/pythonHelper_test.py
+++ test/sol/utils/pythonHelper_test.py
@@ -1,10 +1,13 @@
 import pytest
-from sol.utils.pythonHelper import tup2str, str2tup
+
 import sol.utils.pythonHelper as ph
+from sol.utils.pythonHelper import tup2str, str2tup
+
 
 def testConverters():
-    assert tup2str((1,2,'hi')) == '1_2_hi'
+    assert tup2str((1, 2, 'hi')) == '1_2_hi'
     assert str2tup('3_4') == ('3', '4')
+
 
 def testOneDict():
     d = ph.alwaysOneDict()
@@ -14,3 +17,13 @@ def testOneDict():
     with pytest.raises(TypeError):
         d[2] = -1
     assert d[2] == 1
+
+
+def testListEq():
+    assert ph.listEq([1], [1])
+    assert ph.listEq([], [])
+    assert ph.listEq([2, 2, 2, 2], [2, 2, 2, 2])
+    assert ph.listEq([1], [0x1])
+    assert not ph.listEq([1], [1, 1])
+    assert not ph.listEq([2, 3, 4], [0, 2, 3, 4])
+    assert not ph.listEq([2, 3, 4], [1, 3, 4])
