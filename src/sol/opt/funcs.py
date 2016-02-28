69afb138bbb64fa9483e751fb390eac5baeb2d40
Victor Heorhiadi <victor@cs.unc.edu>
2016-02-27 20:15:55 -0500

    Renamed default capacity functions; getting rid of the dropUpstream function --- not really used anywhere

--- src/sol/opt/funcs.py
+++ src/sol/opt/funcs.py
@@ -6,7 +6,7 @@
 
 
 # noinspection PyUnusedLocal
-def defaultLinkFunc(link, tc, path, resource, linkCaps):
+def defaultLinkCapFunc(link, tc, path, resource, linkCaps):
     """
     Default link function that we use in all the optimizations
 
@@ -35,7 +35,7 @@ def defaultNodeCapFunc(node, tc, path, resource, nodeCaps):
     """
     return tc.volFlows * getattr(tc, '{}Cost'.format(resource)) / nodeCaps[node]
 
-def defaultLinkFuncNoNormalize(link, tc, path, resource):
+def defaultLinkCapFuncNoNorm(link, tc, path, resource):
     """
     Default link function. Computes volume of bytes, does not normalize
 
@@ -48,35 +48,35 @@ def defaultLinkFuncNoNormalize(link, tc, path, resource):
     return tc.volBytes
 
 # noinspection PyUnusedLocal
-def dropUpstreamLinkFunc(link, tc, path, resource, linkCaps, dropRates, cumulative=False):
-    """
-    Example function for modeling link load while taking into account upstream
-    drops
-
-    :param link: the link for which the multiplier is computed
-    :param tc: the traffic class
-    :param path: the path
-    :param resource: resource for which this is being computed
-    :param linkCaps: all of the link capacities
-    :type linkCaps: dict
-    :param dropRates: drop rate (as fraction) at each node (as dict)
-    :type dropRates: dict
-    :param cumulative: If true, all nodes upstream contribute to the drop;
-        if false, only the first node with non-zero drop contributes to the drop
-    :return: volume of traffic in bytes normalized by link capacity, and taking into account upstream drops
-    """
-    retention = 1
-    u, v = link
-    droppedOnce = False
-    for node in path:
-        drop = dropRates.get(node, 0)
-        if drop > 0:
-            droppedOnce = True
-        if not droppedOnce or cumulative:
-            retention -= drop
-        if node == v:
-            break
-    return tc.trafficClass.avgSize * tc.volume * retention / linkCaps[link]
+# def dropUpstreamLinkFunc(link, tc, path, resource, linkCaps, dropRates, cumulative=False):
+#     """
+#     Example function for modeling link load while taking into account upstream
+#     drops
+#
+#     :param link: the link for which the multiplier is computed
+#     :param tc: the traffic class
+#     :param path: the path
+#     :param resource: resource for which this is being computed
+#     :param linkCaps: all of the link capacities
+#     :type linkCaps: dict
+#     :param dropRates: drop rate (as fraction) at each node (as dict)
+#     :type dropRates: dict
+#     :param cumulative: If true, all nodes upstream contribute to the drop;
+#         if false, only the first node with non-zero drop contributes to the drop
+#     :return: volume of traffic in bytes normalized by link capacity, and taking into account upstream drops
+#     """
+#     retention = 1
+#     u, v = link
+#     droppedOnce = False
+#     for node in path:
+#         drop = dropRates.get(node, 0)
+#         if drop > 0:
+#             droppedOnce = True
+#         if not droppedOnce or cumulative:
+#             retention -= drop
+#         if node == v:
+#             break
+#     return tc.trafficClass.avgSize * tc.volume * retention / linkCaps[link]
 
 
 def defaultCostFunction(path):
