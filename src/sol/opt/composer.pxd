af0f70f3b9ebc2b814d33b7d7bf98f45c6076a2b
Victor Heorhiadi <victor@cs.unc.edu>
2016-04-19 18:01:51 -0400

    pxd file for composer. remove relative imports. Simplified composition process, added functions for resource conflict detection

---
+++ src/sol/opt/composer.pxd
@@ -0,0 +1,4 @@
+# coding=utf-8
+from sol.topology.topology cimport Topology
+
+cpdef compose(list apps, Topology topo)
--- src/sol/opt/composer.pyx
+++ src/sol/opt/composer.pyx
@@ -1,14 +1,17 @@
-from gurobipy import LinExpr
+# coding=utf-8
+import itertools
+import operator
+from gurobipy import LinExpr, quicksum
 
-from sol import App
-from sol.opt.varnames import ALLOCATE_FLOW, ROUTE_ALL, xp, CAP_LINKS, CAP_NODES, SHARE_PROPORTIONAL_VOLUME, SHARE_EQUAL, \
-    SHARE_NUM_APPS
-from ..opt import getOptimization
-from ..utils.constansts import *
-from ..utils.exceptions import CompositionError
+from sol.opt.varnames import ALLOCATE_FLOW, ROUTE_ALL, CAP_LINKS, CAP_NODES, MIN_LINK_LOAD, MIN_LATENCY
+from sol.opt.varnames cimport xp
+from sol.topology.topology cimport Topology
+from sol.opt import getOptimization
+from sol.utils.exceptions import CompositionError
 
-#XXX: this is currently tied to Gurobi 
-def addNamedConstraints(opt, app):
+#FIXME: this entire module is currently tied to Gurobi
+
+cdef addNamedConstraints(opt, app):
     opt.addDecisionVars(app.pptc)
     for c in app.constraints:
         if c == ALLOCATE_FLOW:
@@ -19,107 +22,63 @@ def addNamedConstraints(opt, app):
             opt.capLinks(app.pptc, *c[1:])
         elif c[0] == CAP_NODES:
             opt.capNodes(app.pptc, *c[1:])
+        else:
+            raise CompositionError("Unsupported constraint type")
 
-def compose(apps, topo, overlapResMode, resourcesToShare, resShareMode=SHARE_PROPORTIONAL_VOLUME,
-            objShareMode=SHARE_PROPORTIONAL_VOLUME):
+cpdef compose(list apps, Topology topo):
     opt = getOptimization('Gurobi')
     for app in apps:
         opt.addDecisionVars(app.pptc)
         addNamedConstraints(opt, app)
 
-    _composeResources(apps, topo, opt, overlapResMode)
-    if objShareMode == SHARE_PROPORTIONAL_VOLUME:
-        _proportionalFairness(apps, topo, opt)
-    elif objShareMode == SHARE_NUM_APPS:
-        for app in apps:
-            v = addObjVar(app, topo, opt, 1.0/len(apps))
-    elif objShareMode == SHARE_EQUAL:
-        for app in apps:
-            addObjVar(app, topo, opt, 1)
-    else:
-        raise ValueError
-    for r in resourcesToShare:
-        _proportionalFairnessResource(apps, topo, opt, r)
+    _proportionalFairness(apps, topo, opt)
+    _composeResources(apps, topo, opt)
     return opt
 
-def _composeResources(apps, topo, opt, mode):
-    resourceset = set()
-    for a in apps:
-        for r in a.getResourceNames():
-            resourceset.add(r)
+cpdef detectCostConflict(list apps):
+    cdef int i, j
+    for i in range(len(apps)):
+        for j in range(i, len(apps)):
+            sameresoures = set(apps[i].resourceCost.keys()).intersection(apps[j].resourceCost.keys())
+            for r in sameresoures:
+                if apps[i].resourceCost[r] != apps[j].resourceCost[r]:
+                    raise CompositionError("Different costs for resources in overlapping traffic classes")
 
-    # Decide how we pick resource consumption values
-    for r in resourceset:
-        cost = []
-        for app in apps:
-            if app.uses(r):
-                cost.append(app.resources[r])
-        val = None
-        if mode == RES_COMPOSE_MAX:
-            val = max(cost)
-        elif mode == RES_COMPOSE_SUM:
-            val = sum(cost)
-        elif mode == RES_COMPOSE_CONFLICT:
-            if len(set(cost)) != 1:
-                raise CompositionError("Resource composition conflict")
-            val = cost[0]
+cdef _composeResources(list apps, Topology topo, opt):
+    detectCostConflict(apps)
+    for app in apps:
+        for r in app.resourceCost:
+            # TODO: optimize so capacities are not re-computed every time. Compute once for each type of resource
+            nodeCaps = {node: topo.getResources(node)[r] for node in topo.nodes() if r in topo.getResources(node)}
+            linkCaps = {link: topo.getResources(link)[r] for link in topo.links() if r in topo.getResources(link)}
+            print linkCaps
+            opt.consume(app.pptc, r, app.resourceCost[r], nodeCaps, linkCaps)
 
-        nodeCaps = {node: topo.getResources(node)[r].capacity for node in topo.nodes() if r in topo.getResources(node)}
-        linkCaps = {link: topo.getResources(link)[r].capacity for link in topo.links() if r in topo.getResources(link)}
-        for app in apps:
-            if app.uses(r):
-                opt.consume(app.pptc, r, val, nodeCaps, linkCaps)
-
-def _proportionalFairness(apps, topo, opt, to='volume'):
+cdef _proportionalFairness(apps, Topology topo, opt):
     cdef double totalVol = 0
-    if to.lower() == 'volume':
-        vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
-        totalVol = sum(vols.values())
-        for app in apps:
-            v = addObjVar(app, topo, opt)
-            v.Obj = vols[app] / totalVol
-        opt.getGurobiModel().update()
-    else:
-        raise CompositionError('Unknown proportion when using proportional fairness')
+    # Compute proportions according to volume
+    vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
+    totalVol = sum(vols.values())
+    for app in apps:
+        v = addObjVar(app, topo, opt)
+        v.Obj = vols[app] / totalVol
+    opt.getGurobiModel().update()
 
-def _proportionalFairnessResource(apps, topo, opt, resource, to='volume'):
-    cdef double totalVol = 0
-    m = opt.getGurobiModel()
-    if to.lower() == 'volume':
-        vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
-        appToExpr = {}
-        for app in apps:
-            # m.addVar(name='{}_{}'.format(app.name, resource.name))
-            # m.update()
-            e = LinExpr()
-            pptcFiltered = {tc: [p for p in app.pptc[tc] if resource in p] for tc in app.pptc}
-            for tc in pptcFiltered:
-                for path in pptcFiltered[tc]:
-                    e.add(opt.v(xp(tc, path)))
-            appToExpr[app] = e
-        for app1, app2 in zip(appToExpr.keys(), appToExpr.keys()[1:]):
-            m.addConstr(appToExpr[app1] == (vols[app1] / vols[app2]) * appToExpr[app2],
-                        name='FairShare.{}'.format(resource.name))
-            m.update()
-
-def addObjVar(app, topo, opt, weight=0):
+def addObjVar(app, Topology topo, opt, double weight=0):
     if app.obj.lower() == MIN_LINK_LOAD:
         return opt.minLinkLoad('bw', weight)
     elif app.obj.lower() == MIN_LATENCY:
-        return opt.minLatency(topo, app.objpptc, weight)
+        return opt.minLatency(topo, {tc: app.pptc[tc] for tc in app.objTC}, weight)
     else:
         raise CompositionError("Unknown objective")
 
-def getObjVar(app, opt, value=False):
+def getObjVar(app, Topology opt, value=False):
     if app.obj.lower() == MIN_LINK_LOAD:
         return opt.getMaxLinkLoad('bw', value)
     elif app.obj.lower() == MIN_LATENCY:
         return opt.getLatency(value)
     else:
         raise CompositionError("Unknown objective")
-
-def pinTraffic(app1, app2, double ratio):
-    raise NotImplemented
 
 # cpdef maxMinRatio(apps, topo, opt):
 #     m = opt.getGurobiModel()
