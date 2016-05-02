ac3327949317be4dc44be29f0c16b871835f5721
Victor Heorhiadi <victor@cs.unc.edu>
2016-05-02 11:29:36 -0400

    Refactored to fit pep8 a little better

--- src/sol/opt/composer.pxd
+++ src/sol/opt/composer.pxd
@@ -2,7 +2,6 @@
 from sol.topology.topology cimport Topology
 
 cpdef compose(list apps, Topology topo)
-cdef addNamedConstraints(opt, app)
-cpdef detectCostConflict(list apps)
-cpdef addObjVar(app, Topology topo, opt, double weight=*)
-# cpdef getObjVar(app, Topology opt, value=*)
+cdef add_named_constraints(opt, app)
+cpdef _detect_cost_conflict(list apps)
+cpdef add_obj_var(app, Topology topo, opt, double weight=*)
--- src/sol/opt/composer.pyx
+++ src/sol/opt/composer.pyx
@@ -1,19 +1,20 @@
 # coding=utf-8
 
-from sol.opt.varnames import ALLOCATE_FLOW, ROUTE_ALL, CAP_LINKS, CAP_NODES, MIN_LINK_LOAD, MIN_LATENCY
+from sol.opt.varnames import ALLOCATE_FLOW, ROUTE_ALL, CAP_LINKS, CAP_NODES, \
+    MIN_LINK_LOAD, MIN_LATENCY
 from sol.topology.topology cimport Topology
 from sol.opt.gurobiwrapper cimport OptimizationGurobi
 from sol.utils.exceptions import CompositionError
 
 #FIXME: this entire module is currently tied to Gurobi
 
-cdef addNamedConstraints(opt, app):
+cdef add_named_constraints(opt, app):
     opt.addDecisionVars(app.pptc)
     for c in app.constraints:
         if c == ALLOCATE_FLOW:
             opt.allocateFlow(app.pptc)
         elif c == ROUTE_ALL:
-            opt.routeAll(app.pptc)
+            opt.route_all(app.pptc)
         elif c[0] == CAP_LINKS:
             opt.capLinks(app.pptc, *c[1:])
         elif c[0] == CAP_NODES:
@@ -22,49 +23,60 @@ cdef addNamedConstraints(opt, app):
             raise CompositionError("Unsupported constraint type")
 
 cpdef compose(list apps, Topology topo):
+    """
+    Compose multiple applications into a single optimization
+    :param apps: a list of App objects
+    :param topo: Topology
+    :return:
+    """
     opt = OptimizationGurobi()
     for app in apps:
         opt.addDecisionVars(app.pptc)
-        addNamedConstraints(opt, app)
+        add_named_constraints(opt, app)
 
-    _proportionalFairness(apps, topo, opt)
-    _composeResources(apps, topo, opt)
+    _prop_fair_obj(apps, topo, opt)
+    _compose_resources(apps, topo, opt)
     return opt
 
-cpdef detectCostConflict(list apps):
+cpdef _detect_cost_conflict(list apps):
     cdef int i, j
     for i in range(len(apps)):
         for j in range(i, len(apps)):
-            sameresoures = set(apps[i].resourceCost.keys()).intersection(apps[j].resourceCost.keys())
+            sameresoures = set(apps[i].resourceCost.keys()).intersection(
+                apps[j].resourceCost.keys())
             for r in sameresoures:
                 if apps[i].resourceCost[r] != apps[j].resourceCost[r]:
-                    raise CompositionError("Different costs for resources in overlapping traffic classes")
+                    raise CompositionError(
+                        "Different costs for resources in overlapping traffic classes")
 
-cdef _composeResources(list apps, Topology topo, opt):
-    detectCostConflict(apps)
+cdef _compose_resources(list apps, Topology topo, opt):
+    _detect_cost_conflict(apps)
+    nodeCaps = {node: topo.getResources(node) for node in topo.nodes()}
+    linkCaps = {link: topo.getResources(link) for link in topo.links()}
     for app in apps:
         for r in app.resourceCost:
-            # TODO: optimize so capacities are not re-computed every time. Compute once for each type of resource
-            nodeCaps = {node: topo.getResources(node)[r] for node in topo.nodes() if r in topo.getResources(node)}
-            linkCaps = {link: topo.getResources(link)[r] for link in topo.links() if r in topo.getResources(link)}
-            print linkCaps
-            opt.consume(app.pptc, r, app.resourceCost[r], nodeCaps, linkCaps)
+            opt.consume(app.pptc, r, app.resourceCost[r],
+                        {n: nodeCaps[n][r] for n in nodeCaps if
+                         r in nodeCaps[n]},
+                        {l: linkCaps[l][r] for l in linkCaps if
+                         r in linkCaps[l]})
 
-cdef _proportionalFairness(apps, Topology topo, opt):
+cdef _prop_fair_obj(apps, Topology topo, opt):
     cdef double totalVol = 0
     # Compute proportions according to volume
     vols = {app: sum([tc.volFlows for tc in app.pptc]) for app in apps}
     totalVol = sum(vols.values())
     for app in apps:
-        v = addObjVar(app, topo, opt)
+        v = add_obj_var(app, topo, opt)
         v.Obj = vols[app] / totalVol
-    opt.getGurobiModel().update()
+    opt.get_gurobi_model().update()
 
-cpdef addObjVar(app, Topology topo, opt, double weight=0):
+cpdef add_obj_var(app, Topology topo, opt, double weight=0):
     if app.obj.lower() == MIN_LINK_LOAD:
         return opt.minLinkLoad('bw', weight)
     elif app.obj.lower() == MIN_LATENCY:
-        return opt.minLatency(topo, {tc: app.pptc[tc] for tc in app.objTC}, weight)
+        return opt.minLatency(topo, {tc: app.pptc[tc] for tc in app.objTC},
+                              weight)
     else:
         raise CompositionError("Unknown objective")
 
