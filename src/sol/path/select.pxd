f6cf3858fafc53519ef0c2de2285029eb6ed88af
Victor Heorhiadi <victor@cs.unc.edu>
2016-05-02 12:50:03 -0400

    Pep8 fixes to select; new todos and typo fixes.

--- src/sol/path/select.pxd
+++ src/sol/path/select.pxd
@@ -2,4 +2,7 @@
 
 from sol.topology.topology cimport Topology
 
-cpdef selectRobust(apps, Topology topo)
+cpdef choose_rand(dict pptc, int num_paths)
+cpdef sort_paths_per_commodity(dict pptc, key=None, bool inplace=True)
+cpdef select_optimal(apps, Topology topo)
+cpdef select_robust(apps, Topology topo)
--- src/sol/path/select.pyx
+++ src/sol/path/select.pyx
@@ -8,32 +8,29 @@ from collections import defaultdict
 from sol.opt.composer cimport compose
 from sol.topology.topology cimport Topology
 from cpython cimport bool
-from sol.utils.pythonHelper import Tree
 from sol.utils.exceptions import InvalidConfigException
-
-from tmgen cimport TrafficMatrix
 
 _RANDOM = ['random', 'rand']
 _SHORTEST = ['shortest', 'short', 'kshortest', 'k-shortest', 'kshort',
              'k-short']
 
-cpdef chooserand(dict pptc, int numPaths):
+cpdef choose_rand(dict pptc, int num_paths):
     """ Chooses a number of paths uniformly at random
 
     :param pptc: paths per commodity
-    :param numPaths: number of paths to pick per commodity
+    :param num_paths: number of paths to pick per commodity
     :return: the new (chosen) paths per each commodity
     :rtype: dict
     """
     newppk = {}
     for comm in pptc:
-        if len(pptc[comm]) > numPaths:
-            newppk[comm] = random.sample(pptc[comm], numPaths)
+        if len(pptc[comm]) > num_paths:
+            newppk[comm] = random.sample(pptc[comm], num_paths)
         else:
             newppk[comm] = pptc[comm]
     return newppk
 
-cpdef sortPathsPerCommodity(dict pptc, key=None, bool inplace=True):
+cpdef sort_paths_per_commodity(dict pptc, key=None, bool inplace=True):
     """
     Sort paths per commodity
 
@@ -53,35 +50,37 @@ cpdef sortPathsPerCommodity(dict pptc, key=None, bool inplace=True):
             newppk[tc] = sorted(pptc[tc], key=key)
         return newppk
 
-cpdef kShortestPaths(pptc, int numPaths, bool needsSorting=True, bool inplace=True):
+cpdef k_shortest_paths(pptc, int num_paths, bool needs_sorting=True,
+                       bool inplace=True):
     """ Chooses K shortest paths
     :param pptc: paths per commodity
-    :param numPaths: number of paths to choose (k) per commodity
-    :param needsSorting: whether we need to sort the paths first
-    :param inplace: if *needsSorting* is True, whether to sort the ppk in
+    :param num_paths: number of paths to choose (k) per commodity
+    :param needs_sorting: whether we need to sort the paths first
+    :param inplace: if *needs_sorting* is True, whether to sort the ppk in
         place or make a copy
     :return: the new (chosen) paths per commodity
     :rtype: dict
     """
     newppk = None
-    if needsSorting:
-        newppk = sortPathsPerCommodity(pptc, key=len, inplace=inplace)
+    if needs_sorting:
+        newppk = sort_paths_per_commodity(pptc, key=len, inplace=inplace)
     if newppk is None:
         newppk = pptc
     result = {}
     for comm in pptc:
-        result[comm] = newppk[comm][:numPaths]
+        result[comm] = newppk[comm][:num_paths]
     return result
 
-cdef filterPaths(dict pptc, func):
-    """
-    Filter paths using a function.
+def filter_paths(dict pptc, func):
+    """ Filter paths using a function.
 
     :param pptc: paths per traffic class
     :param func: function to be applied to each path
     :return: new paths per commodity with paths for which *func* returned a
         true value
     """
+
+    # TODO: check that this is even used
     assert (hasattr(func, '__call__'))  # ensure this is a function
     result = defaultdict(lambda: [])
     for tc in pptc:
@@ -90,13 +89,13 @@ cdef filterPaths(dict pptc, func):
                 result[tc].append(path)
     return result
 
-def getSelectFunction(strName, kwargs=None):
+def get_select_function(name, kwargs=None):
     """
     Return the path selection function based on name.
     Allows passing of additional keyword arguments, so that the returned function can satisfy the following signature::
         function(pptc, selectNumber)
 
-    :param strName: the name of the function
+    :param name: the name of the function
     :param kwargs: a dictionary of keyword arguements to be passed to the function
     :return: the callable object with
     :raise: InvalidConfigException
@@ -104,7 +103,7 @@ def getSelectFunction(strName, kwargs=None):
 
     Supported names so far: 'random' and 'shortest' For example::
 
-        f = getSelectFunction('random')
+        f = get_select_function('random')
         pptc = f(pptc, 5)
 
     will give you 5 paths per traffic class, randomly chosen
@@ -112,33 +111,38 @@ def getSelectFunction(strName, kwargs=None):
     """
     if kwargs is None:
         kwargs = {}
-    if strName.lower() in _RANDOM:
-        return functools.partial(chooserand, **kwargs)
-    elif strName.lower() in _SHORTEST:
-        return functools.partial(kShortestPaths, **kwargs)
+    if name.lower() in _RANDOM:
+        return functools.partial(choose_rand, **kwargs)
+    elif name.lower() in _SHORTEST:
+        return functools.partial(k_shortest_paths, **kwargs)
     else:
         raise InvalidConfigException("Unknown select method")
 
-cdef dict getPathsBin(app, model):
-    cdef int i
-    pptc = {}
-    tree = Tree()
-    for var in model.getVars():
-        b, tc, ind = var.varName.split('_')
-        tree[tc][int(ind)] = var.x
-    for tc in app.pptc:
-        pptc[tc] = [p for i, p in enumerate(app.pptc[tc]) if tree[tc.ID][i] == 1]
-    return pptc
+cpdef select_optimal(apps, Topology topo):
+    """
+    Select paths to be used for given apps on a given topology.
+    This is equivalent to the optimal solution.
 
-cpdef selectOptimal(apps, Topology topo):
+    :param apps: list of applications
+    :param topo: network topology
+    :return:
+    """
     opt = compose(apps, topo)
     opt.solve()
     # Return paths
-    # TODO: return them per app
+    # FIXME: return them per app
     return opt.getPathsFractions()
 
-cpdef selectRobust(apps, Topology topo):
+cpdef select_robust(apps, Topology topo):
+    """
+
+    :param apps:
+    :param topo:
+    :return:
+    """
     opt = compose(apps, topo)
-    opt.selectPaths((topo.get_num_nodes() - 1)**2  * 10)
+    opt.cap_num_paths((topo.num_nodes() - 1) ** 2 * 10)
     opt.solve()
-    # TODO: return
+    # return paths
+    # FIXME: return them per app
+    return opt.get_chosen_paths()
