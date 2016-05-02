dfe29eb76f3cc174362fc33d3734db39810b387a
Victor Heorhiadi <victor@cs.unc.edu>
2016-05-02 14:20:16 -0400

    Fixed variable usage in watch script

--- watch.sh
+++ watch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 platform=$(uname -s)
-if [ $platform == "Darwin" ]; then
+if [ ${platform} == "Darwin" ]; then
     if [[ "$(which fswatch)" == *"not found"* ]]; then
         echo "Error: You need fswatch for this to work"
         exit 1
     fi
     pip install -e .
-    fswatch -or -l 1 $(find src \( -name '*.pyx' -or -name '*.pxd' \) ) | while read num; do echo $num; pip install -e . ; done
+    fswatch -or -l 1 $(find src \( -name '*.pyx' -or -name '*.pxd' \) ) | while read num; do echo ${num}; pip install -e . ; done
 else
     echo "Sorry, have not implemented this for your OS"
     exit 1
