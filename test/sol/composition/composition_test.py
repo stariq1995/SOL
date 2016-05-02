fceb3f73c634d67a6c84377d34269433b0012c4a
Victor Heorhiadi <victor@cs.unc.edu>
2016-05-02 11:30:03 -0400

    Refactoring to composition tests

--- test/sol/composition/composition_test.py
+++ test/sol/composition/composition_test.py
@@ -2,18 +2,18 @@
 import pytest
 
 from sol.opt.app import App
-from sol.opt.composer import detectCostConflict, CompositionError
+from sol.opt.composer import _detect_cost_conflict, CompositionError
 
 
 def test_ResourceConflictDetection():
     a1 = App({}, [], {'r1': 200, 'r2': 300})
     a2 = App({}, [], {'r1': 200, 'r2': 300, 'r3': 500})
-    detectCostConflict([a1, a2])
-    detectCostConflict([a1])
-    detectCostConflict([])
+    _detect_cost_conflict([a1, a2])
+    _detect_cost_conflict([a1])
+    _detect_cost_conflict([])
     with pytest.raises(TypeError):
-        detectCostConflict({})
+        _detect_cost_conflict({})
     a1 = App({}, [], {'r1': 200, 'r2': 300})
     a2 = App({}, [], {'r1': 200, 'r2': 1, 'r3': 500})
     with pytest.raises(CompositionError):
-        detectCostConflict([a1, a2])
+        _detect_cost_conflict([a1, a2])
