36fd7b981dc06a6877dd0fbe0ea37544f29845d1
Victor H <victor@cs.unc.edu>
2016-05-17 18:47:12 -0400

    added a quick logger. moved away from print statements.

---
+++ src/sol/utils/logger.py
@@ -0,0 +1,7 @@
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+__all__ = ['logger']
