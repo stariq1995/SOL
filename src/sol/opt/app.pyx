262d1c9d90196b225f7cc8d4178f30f509d2f5b3
Victor H <victor@cs.unc.edu>
2016-05-17 18:46:14 -0400

    pep8; added support for App class to have tuple objectives (name and resource)

--- src/sol/opt/app.pyx
+++ src/sol/opt/app.pyx
@@ -1,5 +1,8 @@
 # coding=utf-8
+
 import uuid
+
+import numpy as np
 
 
 class App(object):
@@ -7,22 +10,34 @@ class App(object):
         Represents a single network management application. Optimizition is build
         using these applications.
     """
-    def __init__(self, dict pptc, list constraints, dict resourceCost=None,
-                 str obj=None, objTC=None,
-                 str name=''):
+    def __init__(self, dict pptc, list constraints, dict resource_cost=None,
+                 obj=None, obj_tc=None, str name='', *args, **kwargs):
         self.pptc = pptc
+        if obj is not None:
+            assert isinstance(obj, str) or (isinstance(obj, tuple) and
+                                            isinstance(obj[0], str))
         self.obj = obj
-        self.resourceCost = resourceCost
+        self.resourceCost = resource_cost
         self.name = name
         if not self.name:
             self.name = str(uuid.uuid4()).replace('-', '')
         self.constraints = constraints
-        self.objTC = objTC
+        self.objTC = obj_tc
         if self.objTC is None:
             self.objTC = pptc.keys()
+        self.predicate = kwargs.get('predicate')
 
-    def uses(self, str resourceName):
-        return resourceName in self.resourceCost.keys()
+    def uses(self, str resource_name):
+        return resource_name in self.resourceCost.keys()
+
+    def volume(self):
+        return sum([np.sum(tc.volFlows) for tc in self.pptc])
+
+    def objstr(self):
+        if isinstance(self.obj, tuple):
+            return self.obj[0]
+        else:
+            return self.obj
 
     def __repr__(self):
         return '<sol.App {}>'.format(self.name)
