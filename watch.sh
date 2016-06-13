9cffd9fcd8a347efc18ece5456f1562880092fb9
Victor Heorhiadi <victor@cs.unc.edu>
2016-06-13 17:17:12 -0400

    Added linux check to the watch script

--- watch.sh
+++ watch.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 platform=$(uname -s)
-if [ ${platform} == "Darwin" ]; then
+if [ ${platform} == "Darwin" -o ${platform} == "Linux" ]; then
     if [[ "$(which fswatch)" == *"not found"* ]]; then
         echo "Error: You need fswatch for this to work"
         exit 1
     fi
     python setup.py build_ext --inplace
     echo 'done'
-    fswatch -or -l 1 $(find src \( -name '*.pyx' -or -name '*.pxd' \) ) | while read num; do echo ${num}; python setup.py build_ext --inplace; echo 'done' ; done
+    fswatch -or -l 1 $(find . \( -name '*.pyx' -or -name '*.pxd' \) ) | while read num; do
+        echo ${num}; python setup.py build_ext --inplace ; echo 'done';
+    done
 else
     echo "Sorry, have not implemented this for your OS"
     exit 1
