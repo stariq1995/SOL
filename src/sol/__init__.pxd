e4454499d3dc787235d5dc6a5992f1333f98bf9b
Victor Heorhiadi <victor@cs.unc.edu>
2016-06-13 20:31:53 -0400

    Updated package imports

--- src/sol/__init__.pxd
+++ src/sol/__init__.pxd
@@ -1,6 +1,5 @@
 # coding=utf-8
 
-from topology.topologynx cimport Topology
-from path.paths cimport Path, PathWithMbox
-from opt.quickstart cimport *
+from sol.topology.topologynx cimport Topology
+from sol.path.paths cimport Path, PathWithMbox
 from utils.logger import *
--- src/sol/__init__.py
+++ src/sol/__init__.py
@@ -4,9 +4,8 @@
 
 from topology.topologynx import Topology
 from path.paths import Path, PathWithMbox
-from opt.quickstart import *
 from utils.logger import *
 
 
-__all__ = ['opt', 'path', 'sdn', 'topology', 'utils']
+__all__ = ['opt', 'path', 'sdn', 'topology', 'utils', 'logger']
 __version__ = 0.5
