48307076a6a61c375e2bfe95a59874a9944a45d2
Victor Heorhiadi <victor@cs.unc.edu>
2016-07-14 13:41:35 -0400

    Cleaned up imports

--- src/sol/__init__.pxd
+++ src/sol/__init__.pxd
@@ -3,3 +3,4 @@
 from sol.topology.topologynx cimport Topology
 from sol.path.paths cimport Path, PathWithMbox
 from utils.logger import *
+from sol.opt.quickstart import *
--- src/sol/__init__.py
+++ src/sol/__init__.py
@@ -5,6 +5,7 @@
 from topology.topologynx import Topology
 from path.paths import Path, PathWithMbox
 from utils.logger import *
+from .opt.quickstart import *
 
 
 __all__ = ['opt', 'path', 'sdn', 'topology', 'utils', 'logger']
