b45c3197841facfeb64dac483fbe9310d9101b87
Victor Heorhiadi <victor@cs.unc.edu>
2016-09-16 17:10:42 -0400

    Temporarily removing __init__ imports

--- src/sol/__init__.pxd
+++ src/sol/__init__.pxd
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from sol.topology.topologynx cimport Topology
-from sol.path.paths cimport Path, PathWithMbox
-from utils.logger import *
-from sol.opt.quickstart import *
+# from sol.topology.topologynx cimport Topology
+# from sol.path.paths cimport Path, PathWithMbox
+# from utils.logger import *
+# from sol.opt.quickstart import *
--- src/sol/__init__.py
+++ src/sol/__init__.py
@@ -2,10 +2,10 @@
 
 # TODO: define imports
 
-from topology.topologynx import Topology
-from path.paths import Path, PathWithMbox
-from utils.logger import *
-from .opt.quickstart import *
+# from .topology.topologynx import Topology
+# from .path.paths import Path, PathWithMbox
+# from .utils.logger import *
+# from .opt.quickstart import *
 
 
 __all__ = ['opt', 'path', 'sdn', 'topology', 'utils', 'logger']
