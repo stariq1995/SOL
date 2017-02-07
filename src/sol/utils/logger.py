1d11e8ed201f075e0240b7789f24d560ce04d2cc
Victor Heorhiadi <victor@cs.unc.edu>
2017-02-07 13:01:20 -0500

    Logger update: name changed to sol (to encompass the package); added default logger initializer

--- src/sol/utils/logger.py
+++ src/sol/utils/logger.py
@@ -2,8 +2,20 @@
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-from logging import NullHandler
+from logging import NullHandler, StreamHandler
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('sol')
 logger.addHandler(NullHandler())
 __all__ = ['logger']
+
+
+def init_logger(level=logging.DEBUG):
+    """
+    Defuault logger initializer. Uses console output and sets level to DEBUG
+
+    :param level custom logging level
+    :return: the logger instance
+    """
+    logger.addHandler(StreamHandler())
+    logger.setLevel(level)
+    return logger
