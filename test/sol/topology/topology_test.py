aae96f8aea66f5966c7e903730552cc1467abf7e
Victor Heorhiadi <victor@cs.unc.edu>
2017-03-21 13:08:09 -0400

    Updated unit tests for Topology and TrafficClass

--- test/sol/topology/topology_test.py
+++ test/sol/topology/topology_test.py
@@ -2,32 +2,72 @@
 import os
 
 import networkx
-
+from hypothesis import assume
+from hypothesis import given, strategies as st
+from six import u
 from sol.topology.topologynx import Topology
+
 from sol.topology.generators import complete_topology
 from sol.utils.const import SWITCH
-from six import u
 
 
-def testTopologyWriteRead(tmpdir):
+def test_write_read(tmpdir):
+    """Check that writing topology to disk and restoring it produces the expected result"""
     dirname = u(os.path.abspath(tmpdir.dirname))
     topo = complete_topology(5)
+    for l in topo.links():
+        topo.set_resource(l, 'myresource', 4)
     topo.write_graph(dirname + os.path.sep + u'testgml.gml')
     topo2 = Topology(topo.name)
     topo2.load_graph(dirname + os.path.sep + u'testgml.gml')
     assert networkx.is_isomorphic(topo.get_graph(), topo2.get_graph())
-    topo.write_graph(dirname + os.path.sep + u'lalala.gml')
-    topo2.load_graph(dirname + os.path.sep + u'lalala.gml')
-    assert networkx.is_isomorphic(topo.get_graph(), topo2.get_graph())
+    assert topo.name == topo2.name
+
+    # Check that resources are preserved
+    for n in topo.nodes():
+        assert topo.get_resources(n) == topo2.get_resources(n)
+    for l in topo.links():
+        assert topo.get_resources(n) == topo2.get_resources(n)
 
 
-def testGetNumNodes():
-    topo = complete_topology(8)
-    assert topo.num_nodes() == 8
-    assert topo.num_nodes(SWITCH) == 8
-    assert topo.num_nodes(u'blah') == 0
+@given(st.integers(0, 100), st.text().filter(lambda x: x != SWITCH))
+def test_num_nodes(size, some_text):
+    """
+    Check that num_nodes functions correctly regardless of topology size and service type
+    :param size: size of the topology
+    :param some_text: service type to test the absense of. So anything except 'switch'
+    :return:
+    """
+    assume(some_text)
+    topo = complete_topology(size)
+    assert topo.num_nodes() == size  # size of the network
+    assert topo.num_nodes(SWITCH) == size  # everyting is a switch
+    assert topo.num_nodes(some_text) == 0  # no other functions present
 
 
-def testServiceTypes():
+def test_graph_directed():
+    """
+    Insure we keep newly constructed topologies as directed graph
+    """
+    topo = complete_topology(5)
+    assert isinstance(topo.get_graph(), networkx.DiGraph)
+    # even if original graph is undirected
+    topo = Topology('noname', networkx.star_graph(8))
+
+
+def test_set_service_types():
     # todo: test code that deals with service types & middleboxes
+    pass
+
+
+# TODO: finish other topology tests
+def test_set_resources():
+    pass
+
+
+def test_json_encode_decode():
+    pass
+
+
+def test_from_json():
     pass
--- test/sol/topology/traffic_test.py
+++ test/sol/topology/traffic_test.py
@@ -1,28 +1,74 @@
 # coding=utf-8
 
-import pytest
 import copy
 
+import pytest
+from numpy import array
 from sol.topology.traffic import TrafficClass
 
 
-def test_traffic_class():
-    # Check for getters correctness and sets equivalence of traffic classes
-    t = TrafficClass(1, u'web', 1, 2)
-    t2 = TrafficClass(2, u'web', 1, 2)
-    t3 = TrafficClass(1, u'ssh', 2, 1)
-    t4 = TrafficClass(1, u'web', 1, 2)
-    assert t == t
-    assert t == t4
-    assert copy.copy(t) == t
-    assert t != t3
-    assert t != t2
-    assert t.iepair() == (1, 2)
-    assert str(t) == u'TrafficClass 1 -> 2, web, ID=1'
-    with pytest.raises(AttributeError):
-        print (t3.myval)
-    assert t != u'randomstring'
-    assert not t == u'randomlkdjf;aljkd'
+@pytest.fixture(scope='function')
+def t():
+    return TrafficClass(1, u'web', 1, 2)
 
-    # TODO: include tests for other members: mainly IP prefixes and ports
-    # TODO: Test volume masking
+
+@pytest.fixture(scope='function')
+def t2():
+    return TrafficClass(2, u'web', 1, 2)
+
+
+@pytest.fixture(scope='function')
+def t3():
+    return TrafficClass(1, u'ssh', 2, 1)
+
+
+def test_traffic_class_eq(t, t2, t3):
+    """
+    Checks traffic class equality. Traffic class ID is the only unique identifier
+    of the class. This allows optimization to simplify some intertal logic by using
+    vectorized operations across all traffic classes/paths/epochs
+    """
+    # Check for correctness of getters and equivalence of traffic classes
+    assert t == t
+    assert t == t3
+    assert t != t2
+    assert copy.copy(t) == t
+    assert t != u'randomstring :)'
+    assert t != 0
+    assert t != ''
+    assert t is not None
+
+
+def test_traffic_class_hash(t, t2, t3):
+    """
+    Checks the implementation of traffic class hashing
+    """
+    d = {}
+    # two classes should hash to the same instance
+    d[t] = 0
+    d[t3] = 3
+    assert len(d) == 1  # thus len == 1
+    assert d[t] == 3  # later value is correct
+
+    # and finally check that the ID is what gets hashed
+    t.name = 'abcdef'
+    t.src = -900
+    t.dst = 0
+    t.volFlows = array([2000])
+    d[t] = 90
+    assert d[t3] == 90
+
+
+def test_traffic_class_getters(t, t2, t3):
+    """
+    Ensure that we have i/e getters
+    """
+    assert t.iepair() == (1, 2)
+    assert t.ingress() == 1
+    assert t2.egress() == 2
+
+
+def test_volume_masking():
+    pass
+
+# TODO: test encoding and decoding
