4cb0e922cf6c8d112d1ae183497440766ba01fa0
Victor Heorhiadi <victor@cs.unc.edu>
2017-05-01 12:07:28 -0400

    Added a solution object

---
+++ src/sol/opt/solution.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+from sol.opt.app import App
+
+ALLAPPS = 'allapps'
+
+
+class Solution(object):
+    """
+    Stores the essential basics of the Optmization solution
+    """
+
+    def __init__(self, opt, apps):
+        # Extract paths (pptc), resource loads, binary variables, (app obectives?)
+        if isinstance(apps, App):
+            apps = [apps]
+        self.objectives = {app.name: opt.get_solved_objective(app) for app in apps}
+        self.objectives[ALLAPPS] = opt.get_solved_objective()
+        self.paths = opt.get_paths()
+        self.binvars = {
+            'nodes': opt.get_enabled_nodes(),
+            'links': opt.get_enabled_links(),
+        }
+        self.loads = opt.get_load_dict()
+
+    def __repr__(self):
+        return "Sol Solution, objective: {}".format(self.objectives[ALLAPPS])
+
+    def to_dict(self):
+        """
+        Return a JSON-compatible representation of the solution object as a dictionary
+        :return: 
+        """
+        json_load_arr = []
+        for resource in self.loads:
+            json_load_arr.append({
+                'resource': resource,
+                'elements': [{
+                    'element': element,
+                    'load': self.loads[resource][element]
+                } for element in self.loads[resource]]
+            })
+        return {
+            'objectives': [{'application': k, 'objective': v} for k, v in self.objectives.items()],
+            'paths': self.paths.json_list(),
+            'enabled': self.binvars,
+            'loads': json_load_arr
+        }
