3b88baf910a60ba9cacae6cfa0996155ea1057a8
Victor Heorhiadi <victor@cs.unc.edu>
2017-08-28 12:46:45 -0400

    Better logging confuration/formatting by default

--- src/sol/utils/logger.py
+++ src/sol/utils/logger.py
@@ -2,20 +2,27 @@
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-from logging import NullHandler, StreamHandler
+from logging import StreamHandler, Formatter
 
 logger = logging.getLogger('sol')
-logger.addHandler(NullHandler())
+_been_initialized = False
 __all__ = ['logger', 'init_logger']
 
 
 def init_logger(level=logging.DEBUG):
     """
-    Defuault logger initializer. Uses console output and sets level to DEBUG
+    Default logger initializer. Uses console output and sets level to DEBUG
 
     :param level custom logging level
     :return: the logger instance
     """
-    logger.addHandler(StreamHandler())
-    logger.setLevel(level)
-    return logger
+    global _been_initialized
+    if not _been_initialized:
+        s = StreamHandler()
+        s.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(s)
+        logger.setLevel(level)
+        _been_initialized = True
+        return logger
+    else:
+        return logger
