0b9b64c46caf9455a77d14ff1e4d6ae8727b92c4
Victor Heorhiadi <victor@cs.unc.edu>
2017-08-28 21:22:15 -0400

    from_app now does not use cost_funcs

--- src/sol/opt/quickstart.py
+++ src/sol/opt/quickstart.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 from numpy import array
-from .gurobiwrapper import OptimizationGurobi
-from sol.utils.const import NODES, LINKS, EpochComposition, ERR_UNKNOWN_MODE, Fairness
+
+from sol.utils.const import NODES, LINKS, EpochComposition, ERR_UNKNOWN_MODE, Fairness, MBOXES
 from sol.utils.exceptions import InvalidConfigException
 from sol.utils.logger import logger
+from .gurobiwrapper import OptimizationGurobi
 
 __all__ = ['from_app']
-
 
 
 def from_app(topo, app, network_config):
@@ -28,13 +28,13 @@ def from_app(topo, app, network_config):
     # load imposed by all traffic classes
     for r in app.resource_cost:
         mode, cost_val, cost_func = app.resource_cost[r]
-        if mode == NODES:
+        if mode == NODES or mode == MBOXES:
             capacities = {n: node_caps[n][r] for n in node_caps if r in node_caps[n]}
         elif mode == LINKS:
             capacities = {n: link_caps[n][r] for n in link_caps if r in link_caps[n]}
         else:
             raise InvalidConfigException(ERR_UNKNOWN_MODE % ('resource owner', mode))
-        opt.consume(app.pptc.tcs(), r, capacities, mode, cost_val, [cost_func])
+        opt.consume(app.pptc.tcs(), r, capacities, mode, cost_val, None)
 
     # Cap the resources, if caps were given
     if network_config is not None:
@@ -43,7 +43,6 @@ def from_app(topo, app, network_config):
             logger.debug('Capping resources')
             for r in caps.resources():
                 opt.cap(r, caps.caps(r))
-
 
     # And add any other constraints the app might desire
     opt.add_named_constraints(app)
